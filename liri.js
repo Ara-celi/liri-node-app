2ce85caa42a82e635fd7cd222e60d656bba58ac3
Guillermo <Guillermo@Guillermos-MacBook-Pro.local>
2016-12-15 18:21:16 -0500

    twitter working and movies

--- liri.js
+++ liri.js
@@ -1,28 +1,31 @@
 //write the code you need to grab the data from keys.js. 
 var keys = require('./keys.js');
-
+var keyword = process.argv[2];
 //requiring twitter npm
 var Twitter = require('twitter');
 
+if (keyword === "my-tweets"){
 //storing the keys in a variable
 var client = new Twitter({
-	consumer_key: process.env.consumer_key,
-  	consumer_secret: process.env.consumer_secret,
-  	access_token_key: process.env.access_token_key,
-  	access_token_secret: process.env.access_token_secret
+	consumer_key: keys.twitterKeys.consumer_key,
+  	consumer_secret: keys.twitterKeys.consumer_secret,
+  	access_token_key: keys.twitterKeys.access_token_key,
+  	access_token_secret: keys.twitterKeys.access_token_secret,
 });
 
-var params = {screen_name: 'celi407'};
+var params = {screen_name: 'celi407', count: 20};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
+    for(var i = 0; i < tweets.length; i++) {
+    	console.log(tweets[i].text);
+    	console.log(tweets[i].created_at);
+
+    }
   }
 });
+};
 
-//     keys.twitterKeys.consumer_key,
- //   	keys.twitterKeys.consumer_secret,
- //  	keys.twitterKeys.access_token_key,
- //  	keys.twitterKeys.access_token_secret,
 
 //=============================================================================
 // node liri.js spotify-this-song '<song name here>'
@@ -30,44 +33,45 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 // 1.Artist(s); 2.The song's name; 3.A preview link of the song from Spotify; 4.The album that the song is from
 // if no song is provided then your program will default to "The Sign" by Ace of Base
 
-// var spotify = require('spotify');
+var spotify = require('spotify');
+var songTitle = process.agrv[3]; 
 
-// spotify.search({ type: 'artist', query: 'dancing in the moonlight' }, function(err, data) {
-//     if ( err ) {
-//         console.log('Error occurred: ' + err);
-//         return;
-//     }
+if (keyword==="spotify-this-song"){
+	spotify.search({ type: 'artist', query: songTitle }, function(err, data) {
+    if ( err ) {
+         console.log('Error occurred: ' + err);
+         return;
+     }
  
-//     console.log()
-// });	
+     console.log()
+ })
+ };
+ 	
  
 //======================================================================================
-function (node liri.js movie-this 'mr+nobody'){
-// This will output the following information to your terminal/bash window:
-// Rotten Tomatoes URL.  
-// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-// It's on Netflix!
 
 var request = require('request');
-var movieName = process.argv[2];
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=&tomatoes=json";
+if( keyword==="movie-this") {
+	var movieName = process.argv[3] || "Mr+Nobody";
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json&tomatoes=true";
 console.log(queryUrl);
 request(queryUrl, function(error, response, body){
 if (!error && response.statusCode ===200){
-console.log("Title: " + JSON.parse(body).Title)
-console.log("Release Year: " + JSON.parse(body).Year)
-console.log("Movie Rating: " + JSON.parse(body).Rated)
-console.log("Country Produced: " + JSON.parse(body).Country)
-console.log("Language: " + JSON.parse(body).Language)
-console.log("Plot: " + JSON.parse(body).Plot)
-console.log("Actors: " + JSON.parse(body).Actors)
-console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Response)
-}else{
-}	
+	var body = JSON.parse(body);
+console.log("Title: " + body.Title);
+console.log("Release Year: " + body.Year);
+console.log("Movie Rating: " + body.Rated);
+console.log("Country Produced: " + body.Country);
+console.log("Language: " + body.Language);
+console.log("Plot: " + body.Plot);
+console.log("Actors: " + body.Actors);
+console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+console.log("Tomato URL: "+ body.tomatoURL);
 }
-}) 
+});
+};
 
 
 
